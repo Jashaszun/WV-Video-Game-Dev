f7badc119f5c184f5c5c497ff2455b87d776542b
Jashaszun <jashaszun123@gmail.com>
2014-11-07 15:37:47 -0800

    IT MOVES
    Finally something moving... arrow keys to move the cylinder.

---
+++ Game/Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	const float VELOCITY = 3;
+
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		float velX = 0, velY = 0;
+		if (Input.GetKey(KeyCode.LeftArrow))
+			velX--;
+		if (Input.GetKey(KeyCode.RightArrow))
+			velX++;
+		if (Input.GetKey(KeyCode.UpArrow))
+			velY++;
+		if (Input.GetKey(KeyCode.DownArrow))
+			velY--;
+
+		Vector3 vel = new Vector3(velX, velY);
+		vel = vel.normalized * VELOCITY;
+
+		transform.position += vel * Time.deltaTime;
+	}
+}
